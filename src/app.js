8c05e795b647c6ac22b7bce858d66c6242e778e7
Hugo Leonardo <huleonardo19@gmail.com>
2022-11-11 14:14:06 -0300

    initial commit

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
-// não remova esse endpoint, é para o avaliador funcionar
+// não remova esse endpoint, é para o avaliador funcion
 app.get('/', (_request, response) => {
   response.send();
 });
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
