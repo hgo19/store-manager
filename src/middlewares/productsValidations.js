5ba7cdd5cecf06da8e75a1c0d84460043c1243be
Hugo Leonardo <huleonardo19@gmail.com>
2022-11-11 17:40:53 -0300

    req feito com cobertura de testes 100% em funcs

---
+++ src/middlewares/productsValidations.js
@@ -0,0 +1,5 @@
+const validateId = (req, res, next) => {
+  const { id } = req.params;
+};
+
+module.exports = { validateId };
--- src/models/connection.js
+++ src/models/connection.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || 3306,
-  user: 'root',
-  password: process.env.MYSQL_ROOT_PASSWORD,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: 'StoreManager',
 });
 
