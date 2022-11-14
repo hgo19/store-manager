6ed1b919c4ec04837e75dda08712ca500e488225
Hugo Leonardo <huleonardo19@gmail.com>
2022-11-14 15:06:44 -0300

    terminado testes controller, req 7 feito

--- tests/unit/controllers/sales.controller.test.js
+++ tests/unit/controllers/sales.controller.test.js
@@ -30,4 +30,22 @@ describe('Testa o modulo de sales controller', function () {
     expect(res.status).to.have.been.calledWith(201);
     expect(res.json).to.have.been.calledWith(newSaleReturn);
   });
+
+  it('Verifica o retorno em caso de produto não encontrado', async function () {
+    Sinon.stub(salesService, 'addNewSale').resolves({ type: 'not.found', message: 'Product not found' });
+
+    const res = {};
+    const req = {
+      body: salesExample
+    };
+
+    res.status = Sinon.stub().returns(res);
+    res.json = Sinon.stub().returns();
+
+
+    await salesController.addNewSale(req, res);
+
+    expect(res.status).to.have.been.calledWith(404);
+    expect(res.json).to.have.been.calledWith({ message: 'Product not found' });
+  });
 });
